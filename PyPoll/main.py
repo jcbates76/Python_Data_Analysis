dadc7b39af889834db7258b1d2da3071c8723728
Jason Bates <94392882+jcbates76@users.noreply.github.com>
2022-01-12 08:57:10 -0600

    Included dataset

---
+++ PyPoll/main.py
@@ -0,0 +1,14 @@
+# Given a set of poll data called election_data.csv
+# Dataset composed of three columns: Voter ID, County, Candidate
+# Analyze the votes and calculate the following:
+#   - The total number of votes cast
+#   - A complete list of candidates who received votes
+#   - The percentage of votes each candidate won
+#   - The total number of votes each candidate won
+#   - The winner of the election based on popular vote
+
+
+
+
+
+
