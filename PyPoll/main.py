3c5e0dfde6012c2a7498aba231624a0c5078dc6f
Jason Bates <94392882+jcbates76@users.noreply.github.com>
2022-01-12 11:03:47 -0600

    Base code complete.  Need formatting and output.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,41 +1,51 @@
-# Given a set of poll data called election_data.csv
-# Dataset composed of three columns: Voter ID, County, Candidate
-# Analyze the votes and calculate the following:
-#   - The total number of votes cast
-#   - A complete list of candidates who received votes
-#   - The percentage of votes each candidate won
-#   - The total number of votes each candidate won
-#   - The winner of the election based on popular vote
+
+
 
 # Import functions
 import os
 import csv
 
+total_votes = 0
+candidate_list = []
+candidate_vote_total = []
+candidate = str
+
+
 # Define the path of the CSV input file
-#csvpath = os.path.join("Resources", "election_data.csv") DEBUG
-file = "\Resources\election_data.csv"
+csvpath = os.path.join("Resources", "election_data.csv")
 
 # Define the path of the TXT output file
-output_path = os.path.join("Analysis", "election_results.txt")
-    
+output_path = os.path.join("Analysis", "election_resuts.txt")
+
 # Perform loop for all records within the CSV dataset
-with open(file, "r", encoding="utf-8-sig") as csvfile:
+with open(csvpath, "r", encoding="utf-8-sig") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     # Ignore the header row in the data set
     next(csvreader, None) 
 
-    print(csvreader)
-    print("Maybe this code will finally work")
+    # Iterate through all rows of the dataset
+    for row in csvreader:
+        
+        # Accumulate total number of votes cast
+        total_votes = total_votes + 1
 
-# Things to do
-# Import and read the file
-# Need several counters
-#   - Total number of votes
-#   - Number of votes by each candidate
-# Track the list of candidates
-# Calculate the percentage won by each candidate (votes for won per candidate divided by total number of votes cast)
-# Determine who has the most votes
+        # Determine if candidate is accounted for in list of candidates
+        candidate = row[2]
+
+        # Check to see if the candidate is in the candidiate list, if not, add them and append the vote total list at the same time
+        if candidate not in candidate_list:
+            candidate_list.append(candidate)
+            candidate_vote_total.append(0)
+            
+        
+        # Increment the vote total for that candidate
+        candidate_index = candidate_list.index(candidate)
+    
+        candidate_vote_total[candidate_index] = candidate_vote_total[candidate_index] + 1
+
+for candidate in candidate_list:
+    print(candidate + ": " + str(candidate_vote_total[candidate_list.index(candidate)]/total_votes) + "(" + str(candidate_vote_total[candidate_list.index(candidate)]) + ")")
 
 
-
+print(total_votes)
