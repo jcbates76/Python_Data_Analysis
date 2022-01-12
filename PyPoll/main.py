f079ae436f488f818cfc808c87cbaed2c9b8aad6
Jason Bates <94392882+jcbates76@users.noreply.github.com>
2022-01-12 09:20:15 -0600

    Added read file code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,8 +7,33 @@
 #   - The total number of votes each candidate won
 #   - The winner of the election based on popular vote
 
+# Import functions
+import os
+import csv
 
+# Define the path of the CSV input file
+csvpath = os.path.join("Resources", "election_data.csv")
 
+# Define the path of the TXT output file
+output_path = os.path.join("Analysis", "election_results.txt")
+
+# Perform loop for all records within the CSV dataset
+with open(csvpath, "r", encoding="utf-8-sig") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Ignore the header row in the data set
+    next(csvreader, None) 
+
+    print(csvreader)
+
+# Things to do
+# Import and read the file
+# Need several counters
+#   - Total number of votes
+#   - Number of votes by each candidate
+# Track the list of candidates
+# Calculate the percentage won by each candidate (votes for won per candidate divided by total number of votes cast)
+# Determine who has the most votes
 
 
 
